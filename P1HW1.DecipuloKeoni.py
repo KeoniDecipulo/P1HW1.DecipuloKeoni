ee89d4bb9c03a2aac0b4060f517aecbbb5f25e74
KeoniDecipulo <166351717+KeoniDecipulo@users.noreply.github.com>
2024-04-07 22:37:28 -0400

    Add files via upload

---
+++ P1HW1.DecipuloKeoni.py
@@ -0,0 +1,15 @@
+#Keoni Decipulo
+#4/7/24
+#P1WH1
+#Using Python's input and print functions with one variable
+#adding math sequences to the same variable, but in differet
+#print functions
+
+Num =int(input('Enter integer:'))
+print(Num)
+print('You entered:', Num)
+print(Num, 'squared is', Num*Num, '!!')
+bum =int(input('Enter another integer:'))
+print(bum)
+print(Num,'+',bum,'is', Num+bum)
+print(Num,'*',bum,'is', Num*bum)
